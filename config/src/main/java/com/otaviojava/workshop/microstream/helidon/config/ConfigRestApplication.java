1bb4cfb501d0723d6c96eb621b267d5e67279e8d
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-05 06:58:11 +0100

    Add config sample
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ config/src/main/java/com/otaviojava/workshop/microstream/helidon/config/ConfigRestApplication.java
@@ -0,0 +1,41 @@
+package com.otaviojava.workshop.microstream.helidon.config;
+
+
+
+import com.otaviojava.workshop.microstream.helidon.config.config.ConfigTestController;
+
+
+
+
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class ConfigRestApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+
+        Set<Class<?>> classes = new HashSet<>();
+
+        // resources
+        classes.add(HelloController.class);
+        
+        
+        classes.add(ConfigTestController.class);
+        
+        
+        
+        
+        
+        return classes;
+    }
+}
---
+++ config/src/main/java/com/otaviojava/workshop/microstream/helidon/config/HelloController.java
@@ -0,0 +1,18 @@
+package com.otaviojava.workshop.microstream.helidon.config;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ *
+ */
+@Path("/hello")
+@Singleton
+public class HelloController {
+
+    @GET
+    public String sayHello() {
+        return "Hello World";
+    }
+}
