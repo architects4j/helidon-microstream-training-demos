0af41f1242f4fb37c538829d8291e85250d82a89
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-16 15:15:57 +0100

    format the code on cdi-se
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- microstream-cdi-se/src/main/java/com/otaviojava/workshop/microstream/helidon/NameCounter.java
+++ microstream-cdi-se/src/main/java/com/otaviojava/workshop/microstream/helidon/NameCounter.java
@@ -10,13 +10,13 @@ package com.otaviojava.workshop.microstream.helidon;
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
- * 
+ *
  * This Source Code may also be made available under the following Secondary
  * Licenses when the conditions for such availability set forth in the Eclipse
  * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
  * with the GNU Classpath Exception which is
  * available at https://www.gnu.org/software/classpath/license.html.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  * #L%
  */
@@ -35,31 +35,27 @@ import one.microstream.integrations.cdi.types.cache.StorageCache;
 
 
 @ApplicationScoped
-public class NameCounter
-{
-	@Inject
-	@StorageCache
-	private Cache<String, Integer> counter;
-	
-	@Store
-	public synchronized int count(final String name)
-	{
-		int counter = this.show(name);
-		counter++;
-		this.counter.put(name, counter);
-		return counter;
-	}
-	
-	public int show(final String name)
-	{
-		return ofNullable(this.counter.get(name)).orElse(0);
-	}
-	
-	public Map<String, Integer> getNames()
-	{
-		
-		final Map<String, Integer> map = new HashMap<>();
-		this.counter.forEach(c -> map.put(c.getKey(), c.getValue()));
-		return map;
-	}
+public class NameCounter {
+    @Inject
+    @StorageCache
+    private Cache<String, Integer> counter;
+
+    @Store
+    public synchronized int count(final String name) {
+        int counter = this.show(name);
+        counter++;
+        this.counter.put(name, counter);
+        return counter;
+    }
+
+    public int show(final String name) {
+        return ofNullable(this.counter.get(name)).orElse(0);
+    }
+
+    public Map<String, Integer> getNames() {
+
+        final Map<String, Integer> map = new HashMap<>();
+        this.counter.forEach(c -> map.put(c.getKey(), c.getValue()));
+        return map;
+    }
 }
--- microstream-cdi-se/src/main/java/com/otaviojava/workshop/microstream/helidon/NamesService.java
+++ microstream-cdi-se/src/main/java/com/otaviojava/workshop/microstream/helidon/NamesService.java
@@ -12,13 +12,13 @@ import java.util.Set;
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
- * 
+ *
  * This Source Code may also be made available under the following Secondary
  * Licenses when the conditions for such availability set forth in the Eclipse
  * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
  * with the GNU Classpath Exception which is
  * available at https://www.gnu.org/software/classpath/license.html.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  * #L%
  */
@@ -30,20 +30,17 @@ import one.microstream.integrations.cdi.types.Store;
 
 
 @ApplicationScoped
-public class NamesService
-{
-	@Inject
-	private Names names;
-	
-	@Store
-	public void add(final String name)
-	{
-		this.names.add(name);
-	}
-	
-	public Set<String> getNames()
-	{
-		return this.names.get();
-	}
-	
+public class NamesService {
+    @Inject
+    private Names names;
+
+    @Store
+    public void add(final String name) {
+        this.names.add(name);
+    }
+
+    public Set<String> getNames() {
+        return this.names.get();
+    }
+
 }
