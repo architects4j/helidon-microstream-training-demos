976612555ecee59aad7dc50c790bfa464999c477
Otavio Santana <otaviopolianasantana@gmail.com>
2022-06-29 07:40:48 +0100

    Add project with fault-tolerance
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ fault-tolerance/src/main/java/org/a4j/workshop/microstream/helidon/fault/tolerance/FaulttoleranceRestApplication.java
@@ -0,0 +1,41 @@
+package org.a4j.workshop.microstream.helidon.fault.tolerance;
+
+
+
+
+import org.a4j.workshop.microstream.helidon.fault.tolerance.resilient.ResilienceController;
+
+
+
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class FaulttoleranceRestApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+
+        Set<Class<?>> classes = new HashSet<>();
+
+        // resources
+        classes.add(HelloController.class);
+        
+        
+        
+        classes.add(ResilienceController.class);
+        
+        
+        
+        
+        return classes;
+    }
+}
